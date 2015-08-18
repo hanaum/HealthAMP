2542e335bef45b60fffade7197ef66180cb7b144
Mike Lim <mikelim098098@gmail.com>
2015-08-18 16:08:02 -0700

    resolved conflicts in server Model.js before pushing

--- server/models/Model.js
+++ server/models/Model.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+<<<<<<< HEAD
 var userSchema = new mongoose.Schema({
 	name: String,
     	password: String,
@@ -8,4 +9,45 @@ var userSchema = new mongoose.Schema({
     	Age: Number,
     	Weight: Number
 })
-mongoose.model('User', userSchema);
+
+var UserSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  age: Number,
+  weight: Number,
+  tracked_weight: Number,
+  plans: [{type: Schema.Types.ObjectId, ref: 'Plan'}],
+  created_at: {type: Date, default: new Date},
+  updated_at: {type: Date, default: new Date}
+}, {versionKey: false});
+
+var PlanSchema = new mongoose.Schema({
+  _user: {type: Schema.ObjectId, ref: 'User'},
+  title: String,
+  description: String,
+  todo: [{type: Schema.Types.ObjectId, ref: 'Todo'}],
+  created_at: {type: Date, default: new Date},
+  updated_at: {type: Date, default: new Date}
+}, {versionKey: false});
+
+var TodoSchema = new mongoose.Schema({
+  _plan: {type: Schema.ObjectId, ref: 'Plan'},
+  _goals: {type: Schema.ObjectId, ref: 'Goal'},
+  title: String,
+  description: String,
+  created_at: {type: Date, default: new Date},
+  updated_at: {type: Date, default: new Date}
+}, {versionKey: false});
+
+var GoalSchema = new mongoose.Schema({
+  todo: [{type: Schema.ObjectId, ref: 'Todo'}],
+  created_at: {type: Date, default: new Date},
+  updated_at: {type: Date, default: new Date}
+}, {versionKey: false});
+
+mongoose.model('User', UserSchema);
+mongoose.model('Plan', PlanSchema);
+mongoose.model('Todo', TodoSchema);
+mongoose.model('Goal', GoalSchema);
+>>>>>>> created and instantiated all the schemas for the database: User, Plan, Todo, Goal
