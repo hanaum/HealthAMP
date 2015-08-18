b37b1bb88aa1accd0a2c7e21f61142c6b6163791
Sim <simranlitt@gmail.com>
2015-08-17 17:33:50 -0700

    added template

---
+++ config/routes.js
@@ -0,0 +1,15 @@
+//var users= require('./../server/controllers/users.js');
+
+
+module.exports = function(app) {
+
+	//app.post('/users', function(req, res){
+    	//console.log('we are routing to the users controller')
+    	//users.add(req, res);
+    //})
+    //app.get('/getuser/:any', function(req,res){
+    	//console.log(' we are in rthe get user')
+		//users.getUser(req, res);
+	//})
+
+}
