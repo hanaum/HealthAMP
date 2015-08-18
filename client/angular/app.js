e215a361c4560b447d9502f426a24594353934ba
Mike Lim <mikelim098098@gmail.com>
2015-08-18 09:21:11 -0700

    app.js: included routes for userdashboard & globaldashboard, created userdashboard.html, globaluserdashboard.html

--- client/angular/app.js
+++ client/angular/app.js
@@ -3,17 +3,30 @@
     var myApp = angular.module('myApp', ['ngRoute']);
     //  use the config method to set up routing:
     myApp.config(function ($routeProvider) {
-
       $routeProvider
         .when('/',{
             templateUrl: 'partials/mainpage.html'
         })
-
         .when('/login',{
             templateUrl: 'partials/login.html'
         })
         .when('/register',{
             templateUrl: 'partials/register.html'
+        })
+        .when('/goal', {
+            templateUrl: 'partials/goal.html'
+        })
+        .when('/todo', {
+            templateUrl: 'partials/todo.html'
+        })
+        .when('/userdashboard', {
+            templateUrl: 'partials/userdashboard.html'
+        })
+        .when('/globaldashboard', {
+            templateUrl: 'partials/globaldashboard.html'
+        })
+        .when('/logout', {
+            templateUrl: 'partials/mainpage.html'
         })
         .otherwise({
           redirectTo: '/'
