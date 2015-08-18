b0e70fae24bd2bf5f48e8b95680f9faf6b9aed2e
Mike Lim <mikelim098098@gmail.com>
2015-08-18 08:26:18 -0700

    edited controllers and factory methods in angular folder

--- client/angular/controllers/controller.js
+++ client/angular/controllers/controller.js
@@ -6,3 +6,10 @@ myApp.controller("registerController", function($scope, mainFactory){
 	$scope.register = function(){
 		mainFactory.register($scope.reg);	
 }
+
+myApp.controller("goalController", function($scope, mainFactory){
+  $scope.addGoal = function() {
+    mainFactory.addGoal($scope.goal);;
+  } 
+
+}
--- client/angular/factories/factory.js
+++ client/angular/factories/factory.js
@@ -3,8 +3,17 @@ myApp.factory("mainFactory", function(){
 	
 	factory.register = function(data){
 		$http.post('/register', data). success(function(){
-			
-	}
+    })
+	};
+
+  factory.addGoal = function(data) {
+    $http.post('', data).success(function(){
+    })
+
+  factory.addTodo = function(data) {
+    $http.post('', data).success(function(){
+    })
+  }
 	
 	return factory;
 });
