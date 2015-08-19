1f597f9d48145ae4df927e76175a947d8e18cde1
Hana Um <hanau12@gmail.com>
2015-08-18 17:06:56 -0700

    final body image added with modals

--- client/angular/controllers/controller.js
+++ client/angular/controllers/controller.js
@@ -1,37 +1,35 @@
 myApp.controller("loginController",function($scope){
-<<<<<<< HEAD
-	
-=======
+
 	///Stuff goes here
->>>>>>> added factory methods getOneUser, getAllUsers
-});	
+ //added factory methods getOneUser, getAllUsers
+});
 
 myApp.controller("registerController", function($scope, mainFactory){
 	$scope.register = function(){
 		console.log($scope.reg);
-		mainFactory.register($scope.reg);	
+		mainFactory.register($scope.reg);
+  }
 });
 
 myApp.controller("goalController", function($scope, mainFactory){
   $scope.addGoal = function() {
-    mainFactory.addGoal($scope.goal);;
-  } 
+    mainFactory.addGoal($scope.goal);
+  }
 });
 
 myApp.controller("todoController", function($scope, mainFactory){
   $scope.addTodo = function() {
-    mainFactory.addTodo($scope.todo) {
-    }
+    mainFactory.addTodo($scope.todo);
   }
 });
 
-myApp.controller("userDashboardController", function($scope, mainFactory){
-  mainFactory.getOneUser()
-});
+// myApp.controller("userDashboardController", function($scope, mainFactory){
+//   mainFactory.getOneUser()
+// });
 
-myApp.controller("globalDashboardController", function($scope, mainFactory) {
-  mainFactory.getAllUsers()
-});
-  
+// myApp.controller("globalDashboardController", function($scope, mainFactory) {
+//   mainFactory.getAllUsers()
+// });
 
-});
+
+// });
