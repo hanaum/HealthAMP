4882fd09893f3deff22a6308f8af21383c347e69
Mike Lim <mikelim098098@gmail.com>
2015-08-19 17:26:55 -0700

    connecting mongoose to mongolab, the online mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var fs = require('fs');
 //connect to the database
 //remember to change the database name
-mongoose.connect('mongodb://localhost/health');
+// mongoose.connect('mongodb://localhost/health');
+// mongoose.connect('mongodb://ds055842.mongolab.com:55842/healthamp');
+mongoose.connect('mongodb://healthamp:healthamp123@ds055842.mongolab.com:55842/healthamp');
 
 //loads all of the model files
-var models_path = __dirname + '/../server/models'
+var models_path = __dirname + '/../server/models';
 //for each file in the path
 fs.readdirSync(models_path).forEach(function(file){
 	//check if it is a js file, if so load it
@@ -13,4 +15,4 @@ fs.readdirSync(models_path).forEach(function(file){
 		//load each model file
 		require(models_path + '/'+ file);
 	}
-})
+});
